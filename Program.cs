99a93f8cd9d57da4edbe79476fa9ba493f19f40d
Debora <deehbarreto7@gmail.com>
2026-04-24 22:05:24 -0300

    calculando tempo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿double tamanhoArquivo, velocidadeConexao;
+double tempoEstimado;
+
+Console.WriteLine ("--- Tempo de Download ---");
+
+Console.WriteLine ("Tamanho do arquivo em MB........: ");
+tamanhoArquivo = Convert.ToDouble (Console.ReadLine())*8;
+
+Console.WriteLine("Velocidade da conexão em Mbps...:");
+velocidadeConexao = Convert.ToDouble (Console.ReadLine());
+
+tempoEstimado = tamanhoArquivo / velocidadeConexao /60;
+
+Console.Write($"\ntempoEstimado: {tempoEstimado:N2} minutos");
